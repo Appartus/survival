9c35fb069371fb595256b2f49ba94166637c77b0
Kate <corinajasmine628@gmail.com>
2020-01-06 23:55:54 +0100

    zbieranie ogniw, dodanie HUD i generatora

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public static int charge = 0;
+
+    public AudioClip collectSound;
+
+    // HUD
+    public Texture2D[] hudCharge;
+    public RawImage chargeHudGUI;
+    // Generator
+    public Texture2D[] meterCharge;
+    public Renderer meter;
+    void Start()
+    {
+        charge = 0;
+    }
+    void CellPickup()
+    {
+        HUDon();
+        AudioSource.PlayClipAtPoint(collectSound, transform.position);
+
+        charge++;
+        chargeHudGUI.texture = hudCharge[charge];
+
+        meter.material.mainTexture = meterCharge[charge];
+
+    }
+    void HUDon()
+    {
+
+        if (!chargeHudGUI.enabled)
+        {
+
+            chargeHudGUI.enabled = true;
+
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+
+}
